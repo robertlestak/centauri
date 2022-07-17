22811c592ac0e25b9ee00b8c22520884ffbbbb35
robertlestak <robert.lestak@umusic.com>
2022-07-17 13:47:03 -0700

    multi arch docker container

--- devops/scripts/deploy.sh
+++ devops/scripts/deploy.sh
@@ -4,10 +4,10 @@ set -e
 IMAGE=registry.lestak.sh/centaurid
 TAG=$GIT_COMMIT
 
-docker build -f devops/docker/centaurid.Dockerfile \
+docker buildx build -f devops/docker/centaurid.Dockerfile \
+    --platform=linux/arm64,linux/amd64,linux/arm/v7 \
     -t $IMAGE:$TAG \
+    --push \
     .
-
-docker push $IMAGE:$TAG
 
 sed "s,$IMAGE:.*,$IMAGE:$TAG,g" devops/k8s/*.yaml | kubectl apply -f -
